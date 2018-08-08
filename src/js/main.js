a0b9e0deb6825740df827609696814b26e3b7af4
mrjeffcodes95 <mrjeffcodes95@gmail.com>
2018-08-08 13:15:55 -0500

    everything is ready for the JS

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,11 @@
 function displayQuestions() {
 	var q1 = "How do you solve for the x intercepts of a parabala?"
+	var q2 = "What is the difference between a verb and an adverb?"
+	var q3 = "In 'The Road Not Taken' by Robert Frost, what does Frost mean when he says, 'Oh, I kept the first for another day'?"
+	var q4 = "In HTML5 what is the difference between the 'class' tag and the 'id'asasas tag?"
 
 	document.getElementById('Q1').innerHTML = q1;
+	document.getElementById('Q2').innerHTML = q2;
+	document.getElementById('Q3').innerHTML = q3;
+	document.getElementById('Q4').innerHTML = q4;
 }
